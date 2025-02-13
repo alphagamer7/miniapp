fdc4b6169733a1b379d1e512bb30c391883479cb
Athif Shaffy <athif.shaffy@gmail.com>
2025-02-12 20:40:37 -0500

    Updated alerts

--- src/pages/PhantomWallet/PhantomWalletPage.jsx
+++ src/pages/PhantomWallet/PhantomWalletPage.jsx
@@ -25,6 +25,7 @@ const PhantomWalletConnect = () => {
         const parts = startParam.split('_');
         if (parts[0] === 'w' && parts[1] && parts[2] === 'u' && parts[4] === 's') {
           const walletAddress = parts[1];
+          alert(walletAddress);
           const userId = parts[3];
           const sessionId = parts[5];
           const provider = getProvider();
@@ -42,6 +43,7 @@ const PhantomWalletConnect = () => {
                 type: 'wallet_connected',
                 wallet: parts[1]
               }));
+              alert("wallet connected")
               return;
             } catch (err) {
               console.error('Error connecting to Phantom:', err);

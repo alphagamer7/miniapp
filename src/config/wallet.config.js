713faa76311c596d89c2f287e203f9f80970096c
Athif Shaffy <athif.shaffy@gmail.com>
2025-03-11 21:58:01 -0400

    code refacotred

---
+++ src/config/wallet.config.js
@@ -0,0 +1,15 @@
+export const WALLET_CONFIG = {
+  TELEGRAM_BOT_URL: 'https://t.me/testalphabot44123411bot',
+  REDIRECT_BASE_URL: 'https://thealphanova.com',
+  PHANTOM_CONNECT_URL: 'https://phantom.app/ul/v1',
+  APP_URL: 'https://thealphanova.com/',
+  CLUSTER: 'devnet',
+  STORAGE_KEYS: {
+    WALLET: 'phantomWallet',
+    ENCRYPTED_DATA: 'phantom_encrypted_data',
+    PUBLIC_KEY: 'phantom_public_key',
+    NONCE: 'phantom_nonce',
+    CONNECTION_SECRET_KEY: 'phantom_connection_secret_key',
+    CONNECTION_PUBLIC_KEY: 'phantom_connection_public_key',
+  }
+}; 
